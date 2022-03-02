c6b87761c6fb5c9aef1c192fb5c5fcb1d25d76e5
Trintos <trintos@live.ru>
2022-03-02 22:32:12 +0300

    add ? to string for exception null

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите ваше имя ");
 // ReadLine - при помощи неё происходит считывание данных
 // Требуется указать переменную username для ввода данных и её тип. Это будет string - строка.
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 // Далее выводим данные в консоль
 Console.Write("Привет, ");
 // с указанием данных введённых ранее в переменную
